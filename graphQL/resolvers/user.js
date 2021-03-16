9f77a4c9dbd86e1399a418a4e97d8db854a561a4
sameghosts <seanfeiner.brown@gmail.com>
2021-03-16 16:21:08 -0700

    try catch for apollo error

--- graphQL/resolvers/user.js
+++ graphQL/resolvers/user.js
@@ -1,3 +1,4 @@
+const { ApolloError } = require('apollo-server-errors')
 const bcrypt = require('bcrypt')
 
 
@@ -13,12 +14,29 @@ module.exports = {
     }, {
       User
     }) => {
+      try {
+        let { username, email } = newUser
       // First Check if username is taken
+      let user;
+      user = await User.findOne({ userName: username });
+      if (user){
+        throw new Error("Username is already taken!")
+      }
       // Check if email registered
+      user = await User.findOne({
+        email: email
+      });
+      if (user) {
+        throw new Error("Email is already registered!")
+      }
       // Create new user instance
       // hash the password
       // save the user to db
       // issue the authentication JWT token
+
+      } catch (err) {
+        throw new ApolloError(err.message, 400)
+      }
     }
   }
 }
