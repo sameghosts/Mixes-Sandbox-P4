c5a5aec3e06f6babcbb60dd704217a17ce4b3edf
sameghosts <seanfeiner.brown@gmail.com>
2021-03-17 13:39:08 -0700

    Mixlist stubbing typedef

--- graphQL/typeDefs/mixlist.js
+++ graphQL/typeDefs/mixlist.js
@@ -8,10 +8,37 @@ const { gql } = require('apollo-server-express');
 // ------ Module: mixlist gql module / type node
 const mixlist = gql`
 # First define mixlist type
-
+type MixList {
+  id: ID!
+  name: String!
+  user: String!
+  authorDisplay: String
+  imageUrl: String
+  tagline: String
+  description: String
+  featuredText: String
+  tracks: [Track]
+  comments: [Comment]
+}
 # Mixlist exted queries
 
 # Mixlist extend mutations
 
 # Other types / Inputs
+type Comment {
+  user: User!
+  body: String!
+  date: String!
+
+}
+
+type Track {
+  trackType: String!
+  title: String!
+  artist: String!
+  uri: String!
+  featured: Boolean
+}
 `;
+
+module.exports = mixlist;
