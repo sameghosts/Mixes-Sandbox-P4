8110361585efaabc7cb560fa57db5705db2ee332
sameghosts <seanfeiner.brown@gmail.com>
2021-03-17 10:20:16 -0700

    reg and login working, now modularizing for test, plus docs updated

--- graphQL/typeDefs/user.js
+++ graphQL/typeDefs/user.js
@@ -2,30 +2,14 @@ const { gql } = require('apollo-server-express');
 //TODO: profile??? does it go here or its own graphql schema
 
 //TODO: Migrate from index for module joining, double check it works 
-module.exports =  gql`
-#Base Queries for user
-  extend type Query {
-    hello: String
-    #authUser: User! @isAuth
-    loginUser(username: String!, password: String!):AuthUser!
-  }
-  
-  #create user mutation
-  extend type Mutation {
-    registerUser(newUser: UserInput!): AuthUser!
-  }
+const user = gql`
 
-#Schema for new users
-  input UserInput {
-    email: String!
-    username: String!
-    createdAt: String!
-    updatedAt: String!
-  }
+  # User type for user module base def 
   type User {
     id: ID!
-    userName: String!
+    username: String!
     email: String!
+    avatar: String
     profile: UserProfile
     #userMixes: [MixList]
     #userCollection: Collection
@@ -33,6 +17,34 @@ module.exports =  gql`
     updatedAt: String
   }
 
+  # User Queries
+  type Query {
+    infoUserResolvers: String
+    #authUser: User! @isAuth
+    loginUser(username: String!, password: String!):AuthUser!
+    # Find User by Email
+    # Find User by Username
+  }
+
+  # User Mutations
+  type Mutation{
+    registerUser(newUser: UserInput!): AuthUser!
+  }
+  # TODO: figure out if I will be doing subscriptions
+  # type Subscription {
+  #   _:String
+  # }
+
+  # Input Schema for new users / registration
+  input UserInput {
+    email: String!
+    username: String!
+    password: String!
+    avatar: String
+  }
+
+  # Type Schema User Profile
+  #TODO: Move to its own schema?, or keep as userprofile and also define a profile?
   type UserProfile {
     displayName: String
     bio: String
@@ -40,10 +52,13 @@ module.exports =  gql`
     following: [User]
     followers: [User]
   }
-
+  
+  # Type auth user, for payload 
   type AuthUser {
     user: User!
     token: String!
   }
-
+  
 `;
+
+module.exports = typeDefs;
